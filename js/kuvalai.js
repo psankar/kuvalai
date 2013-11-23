ad6e7358dad2fe12661c2c09a76f6b8e321b21d1
Sankar சங்கர் <sankar.curiosity@gmail.com>
2013-11-23 15:10:49 +0530

    Meaningful alert messages

--- js/kuvalai.js
+++ js/kuvalai.js
@@ -1,15 +1,13 @@
 function executeCode(i) {
 	var slideNumber = "#kuvCode"+i;
 	var sourceCode = $(slideNumber).eq(0).val();
-	alert(sourceCode);
 	var parameters = {"a" : JSON.stringify({ Code: sourceCode})}; 
-	alert(parameters);
 
 	$.getJSON('http://localhost:8080', parameters)
 		.done(function(str){
 			document.getElementById('kuvOutput1').innerHTML = str.Output;
 		})
 	.fail(function(e) {
-		alert("Failure");
+		alert("Error: Check if the compiler server is running correctly.");
 	});
 }
